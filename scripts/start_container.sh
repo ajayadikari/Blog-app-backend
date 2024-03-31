1de32ca910b69313a89419564c8de13dd64a923c
ajay adikari <adikariajaykumar@gmail.com>
2024-03-31 23:30:06 +0530

    small change

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,3 @@
 echo "in start_container_script"
 docker pull ajayadikari/blog-app-backend:latest
-docker run -d -p 8000:8000 ajayadikari/blog-app-backend
+docker run -d -p 8000:8000 ajayadikari/blog-app-backend:latest
