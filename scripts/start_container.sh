35c4a1a5e6d37273765db34a0c70f625527e98cb
Ajay Adikari <94204251+ajayadikari@users.noreply.github.com>
2024-05-30 18:54:01 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ajayadikari/blog-app-backend
+docker pull ajayadikari/blog-app-backend:latest
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 ajayadikari/blog-app-backend
+docker run -d -p 8000:8000 ajayadikari/blog-app-backend:latest
